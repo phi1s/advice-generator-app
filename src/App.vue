ec9541e33ad057a99736a335a1226693246caf1d
philipp <scnp@zhaw.ch>
2023-01-15 23:15:29 +0100

    first version with api

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <AdviceGenerator />
+</template>
+
+<script>
+import AdviceGenerator from "./components/AdviceGenerator.vue";
+
+export default {
+  name: "App",
+  components: {
+    AdviceGenerator,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #cee3e9;
+  background-color: #202733;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  color: #53ffaa;
+}
+</style>
---
+++ src/components/AdviceGenerator.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="AdviceGenerator">
+    <h1>Advice #{{ slip.id }}</h1>
+    <p>{{ slip.advice }}</p>
+    <button @click="getAdvice()">shuffle</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "AdviceGernerator",
+  data() {
+    return {
+      slip: {},
+    };
+  },
+  mounted() {
+    this.getAdvice();
+  },
+
+  methods: {
+    getAdvice() {
+      axios
+        .get("https://api.adviceslip.com/advice")
+        .then((response) => (this.slip = response.data.slip))
+        .catch((err) => {
+          // Handle errors
+          console.error(err);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.AdviceGenerator {
+  width: 540px;
+  height: 332px;
+  background-color: #313a48;
+}
+</style>
