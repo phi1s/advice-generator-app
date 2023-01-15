a9d5ea3a4e09546c917d90ad64f7a60a326e52d0
philipp <scnp@zhaw.ch>
2023-01-15 23:53:56 +0100

    mobile and desktop version created

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap"
+    rel="stylesheet"
+  />
   <AdviceGenerator />
 </template>
 
@@ -15,7 +21,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Manrope", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #cee3e9;
@@ -32,6 +38,37 @@ export default {
   align-items: center;
 }
 h1 {
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 13px;
+  line-height: 18px;
+  text-align: center;
+  letter-spacing: 4.08571px;
   color: #53ffaa;
+  size: 13px;
+  text-transform: uppercase;
+  margin-top: 40px;
+}
+
+p {
+  font-family: "Manrope";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 24px;
+  line-height: 33px;
+  text-align: center;
+  letter-spacing: -0.257143px;
+  color: #cee3e9;
+  padding: 0px 14px;
+}
+
+@media screen and (min-width: 700px) {
+  p {
+    font-size: 28px;
+    line-height: 38px;
+    letter-spacing: -0.3px;
+    margin-bottom: 40px;
+  }
 }
 </style>
--- src/components/AdviceGenerator.vue
+++ src/components/AdviceGenerator.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="AdviceGenerator">
     <h1>Advice #{{ slip.id }}</h1>
-    <p>{{ slip.advice }}</p>
-    <button @click="getAdvice()">shuffle</button>
+    <p>"{{ slip.advice }}"</p>
+    <div class="divider" />
+    <button class="dice" @click="getAdvice()">
+      <img src="../assets/icon-dice.svg" alt="icon dice" />
+    </button>
   </div>
 </template>
 
@@ -35,8 +38,49 @@ export default {
 
 <style scoped>
 .AdviceGenerator {
-  width: 540px;
-  height: 332px;
+  width: 343px;
+  height: 315px;
   background-color: #313a48;
+  position: relative;
+  border-radius: 10px;
+}
+
+button:hover {
+  box-shadow: 0px 0px 40px #53ffaa;
+}
+.divider {
+  width: 295px;
+  height: 16px;
+  margin: auto;
+  background-image: url("../assets/pattern-divider-mobile.svg");
+}
+.dice {
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  background-color: #53ffaa;
+  position: absolute;
+  left: calc(50% - 32px);
+  bottom: -32px;
+}
+img {
+  padding: 20px;
+}
+
+button {
+  border: none;
+  padding: 0;
+}
+
+@media screen and (min-width: 700px) {
+  .AdviceGenerator {
+    width: 540px;
+    height: 332px;
+  }
+  .divider {
+    width: 444px;
+    height: 16px;
+    background-image: url("../assets/pattern-divider-desktop.svg");
+  }
 }
 </style>
